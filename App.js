5283171e68c33cb7e077c19fe05753f1da737c38
Jonathan Kruse <jonathanskruse@gmail.com>
2018-06-12 15:50:48 -0500

    turned back app

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { createBottomTabNavigator } from 'react-navigation'; // Version can be specified in package.json
 
+
+
+
+
 class HomeScreen extends React.Component {
+  
   render() {
     return (
+      
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Home!</Text>
+        
       </View>
+      
     );
   }
 }
@@ -20,6 +28,7 @@ class SettingsScreen extends React.Component {
     );
   }
 }
+
 export default createBottomTabNavigator({
   Home: { screen: HomeScreen },
    Settings: { screen: SettingsScreen },
--- components/LoginPage/loginPage.js
+++ components/LoginPage/loginPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Buttom } from 'react-native';
 
 export default class LoginPage extends React.Component {
     
     render() {
       return (
         <View style={styles.container}>
-          
+         
           <Text>Boom Boom Boom Boom</Text>
         </View>
       );
