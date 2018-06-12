8784db17d86ed59ff1caceaad64741632a1d4f73
Jonathan Kruse <jonathanskruse@gmail.com>
2018-06-11 20:44:35 -0500

    page update

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -6,7 +6,7 @@ class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home!</Text>
+        <Text>Page 1</Text>
       </View>
     );
   }
