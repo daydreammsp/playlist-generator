93a1086ce793207fdddd98828829e35e561875f7
Jonathan Kruse <jonathanskruse@gmail.com>
2018-06-12 20:40:31 -0500

    api call on dom

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { createBottomTabNavigator } from 'react-navigation'; // Version can be specified in package.json
-
+import axios from 'axios';
 
 
 
 
 class HomeScreen extends React.Component {
-  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movies: 'Movies?',
+      
+    };
+  }
+  serverRequest=()=>{
+    axios.get('http://localhost:5002')
+  .then( (response)=> {
+    console.log(response.data);
+    this.setState({
+      movies: response.data
+    })
+  })
+  .catch( (error)=> {
+    console.log(error);
+  });
+  }
   render() {
     return (
       
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Home</Text>
-        
+        <Button title="send Request" onPress={this.serverRequest}></Button>
+      <Text>{this.state.movies}</Text>
       </View>
       
     );
