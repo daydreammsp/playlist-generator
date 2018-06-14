49bcd9f6603bb2376bdb2630270a7273f1059fc0
Jonathan Kruse <jonathanskruse@gmail.com>
2018-06-13 20:40:38 -0500

    axios post works

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { createBottomTabNavigator } from 'react-navigation'; // Version can be specified in package.json
 import axios from 'axios';
 
@@ -12,27 +12,38 @@ class HomeScreen extends React.Component {
 
     this.state = {
       movies: 'Movies?',
+      text: ''
       
     };
   }
-  serverRequest=()=>{
-    axios.get('http://localhost:5002')
+  serverRequest=(text)=>{
+    
+    
+    console.log(text)
+    axios.post('http://localhost:5002/api', [text])
   .then( (response)=> {
     console.log(response.data);
     this.setState({
-      movies: response.data
+      movies: response.data,
+      
     })
   })
   .catch( (error)=> {
     console.log(error);
   });
+  
   }
   render() {
     return (
       
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Home</Text>
-        <Button title="send Request" onPress={this.serverRequest}></Button>
+        <TextInput
+          style={{width: 100, height: 40}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text: text})}
+        />
+        <Button title="send Request" onPress={()=>this.serverRequest(this.state.text)}></Button>
       <Text>{this.state.movies}</Text>
       </View>
       
